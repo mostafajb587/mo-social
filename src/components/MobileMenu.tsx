307d126571b64ee8bf9b100c92918a38e7c3e067
mostafa jbili <mostafajbili23@gmail.com>
2025-08-08 11:07:57 +0300

    feat(components): add MobileMenu component and update NavBar layout for mobile responsiveness

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,11 @@
+export default function MobileMenu() {
+  return (
+    <div>
+      <div className="flex flex-col gap-[4.5] cursor-pointer">
+        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+      </div>
+    </div>
+  );
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+
 export default function NavBar() {
-  return <div>navBar</div>;
+  return (
+    <div className="flex items-center justify-between h-24">
+      {/* left */}
+      <div className="font-bold text-xl text-blue-600">
+        <Link href={"/"}>MoSocial</Link>
+      </div>
+      {/* center */}
+      <div className="hidden"></div>
+      {/* right */}
+      <div>
+        <MobileMenu></MobileMenu>
+      </div>
+    </div>
+  );
 }
